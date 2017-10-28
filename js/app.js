f5407d8e7e2caf41132ec0f778949010e734fa6f
xxiMiaxx <lam3h-sa@live.co.uk>
2017-10-28 19:56:06 +0300

    adding data of places

--- js/app.js
+++ js/app.js
@@ -1,48 +1,40 @@
 var map;
   // we craete a class to instaniate all of the places
-  function places(name, address){
-    var place = this;
-    place.name = name;
-    place.address= address;
-    // place.
-  }
 
 
-// var data = [{
-//   name: "",
-//   address: {
-//     lanttitude:
-//     longitude:
-//   }
-// },
-// {
-//   name: "",
-//   address: {
-//     lanttitude:
-//     longitude:
-//   }
-// },
-// {
-//   name: "",
-//   address: {
-//     lanttitude:
-//     longitude:
-//   }
-// },
-// {
-//   name: "",
-//   address: {
-//     lanttitude:
-//     longitude:
-//   }
-// },
-// {
-//   name: "",
-//   address: {
-//     lanttitude:
-//     longitude:
-//   }
-// }];
+
+function viewModel() {
+  var self= this;
+  self.places = ko.observableArray([
+    new place(
+			"Kingdom Centre",
+			{lat: 24.7111837, lng: 46.67340100000001},
+			"img/Kingdom_Center_.jpg",
+			true),
+		new place(
+			"Al Faisaliyah Center",
+			{lat: 24.6905765, lng: 46.68509700000004},
+			"img/faisaleh.jpg",
+			true),
+		new place(
+			"Burj Rafal",
+			{lat: 24.7925009, lng: 46.632335799999964},
+			"img/Projects-Rafal-02.jpg",
+			true),
+		new place(
+			"KAFD World Trade Center",
+			{lat: 24.76195599999999, lng: 46.64043370000002},
+			"img/1stGrade-Panora Park- Financial Plaza (22)PhotoGala.jpg",
+			true),
+		new place(
+			"Nakheel Tower",
+			{lat: 24.7488774, lng: 46.65273419999994},
+			"img/70659410.jpg",
+			true)
+  ]);
+  self.filter
+}//end
+
 // google map error function
   function mapError(){
     alert('Google Maps is temporairly unavailable. Try again later');
@@ -61,12 +53,53 @@ var map;
 
   }
 
-  function place(name, coordin, wikiTitle, visible){
+  function place(name, coordin, img, visible){
     var self = this;
     self.name= ko.observable(name);
     self.coordin = ko.observable(coordin);
-    self.wikiTitle = ko.observable(wikiTitle);
+    self.img = ko.observable(img);
     self.visible = ko.observable(visible);
     // google maps marker
     self.marker = null;
   }
+
+
+
+  // creating a marker for the place
+  function addMarker(place){
+    var marker = new google.maps.Marker({
+      position: place.coordin,
+      map: map,
+      animation: null
+    });
+
+
+  var infoWindow = new google.maps.InfoWindow({
+    content: '<div class="info"><div class="info-text"><p class="title">'+place.wikiTitle+'</p><p class="address">'+place.name+'</p></div><div class="bus-img"><img src="'+place.img+'" alt="place img"></div>'
+  });
+
+  //click listener
+  marjer.addListener('click', function(){
+    places().forEach(function(place){
+      place.infoWindow.close();
+    });
+    infoWindow.open(map, marker);
+    toggleBounce(marker);
+    setTimeout(function(){
+      marker.setAnimation(null);
+    }, 700);
+  });
+
+  // adding the info window and marker to the place
+  place.infoWindow = infoWindow;
+  place.marker= marker;
+}
+
+//animation function for marker
+function toggleBounce(m){
+  if(m.getAnimation() !== null){
+    m.setAnimation(null);
+  } else {
+    m.setAnimation(google.maps.Animation.BOUNCE)
+  }
+}
