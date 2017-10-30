a556333551e929bea5e05161728679d185061f77
xxiMiaxx <lam3h-sa@live.co.uk>
2017-10-30 10:32:52 +0300

    adding markers and drop down list

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 var map;
   // we craete a class to instaniate all of the places
-
+  function place(name, coordin, img, visible){
+    var self = this;
+    self.name= ko.observable(name);
+    self.coordin = ko.observable(coordin);
+    self.img = ko.observable(img);
+    self.visible = ko.observable(visible);
+    // google maps marker
+    self.markerObject = null;
+  }
 
 
 function viewModel() {
@@ -32,13 +40,35 @@ function viewModel() {
 			"img/70659410.jpg",
 			true)
   ]);
-  self.filter
-}//end
+  self.filterString = ko.observable("");
+  self.moreInfo = function(place){
+    google.maps.event.trigger(place.marker, 'click');
+  }
 
+  self.submit = function(){
+    if(self.filterString() !== ""){
+      var word= self.filter
+    }
+  }
+
+  self.listClick = function(location){
+    //loop in the markers array to match marker with location
+    for ( var i=0; i< markers.length; i++){
+      if(markers[i].loc == location ){
+        markers[i].setAnimation(4);
+        // popUpInfoWindow(location);
+      }
+    }
+  }
+
+}//end
+ko.applyBindings(new viewModel());
 // google map error function
   function mapError(){
     alert('Google Maps is temporairly unavailable. Try again later');
   }
+var markers = [];
+var vm = ko.dataFor(document.body);
 
   function initMap(){
     var mapElemnt = document.getElementById('googleMap');
@@ -50,56 +80,66 @@ function viewModel() {
         position: google.maps.ControlPosition.LEFT_BOTTOM
       }
     });
+    for(var i=0; i<vm.places().length; i++){
+      var loc = vm.places()[i];
+       var marker = new google.maps.Marker({
+        position: loc.coordin(),
+        map: map,
+        loc: loc,
+        animation: null
+      });
+      loc.markerObject = marker;
+      marker.addListener('click', onClickMarker); //<<<<<<<<<
+      markers.push(marker);
+    }
 
   }
 
-  function place(name, coordin, img, visible){
+  function onClickMarker(){
     var self = this;
-    self.name= ko.observable(name);
-    self.coordin = ko.observable(coordin);
-    self.img = ko.observable(img);
-    self.visible = ko.observable(visible);
-    // google maps marker
-    self.marker = null;
+    self.setAnimation(4);
+    popUpInfoWindow(self.loc);
   }
+  //function popUpInfoWindow
 
 
 
-  // creating a marker for the place
-  function addMarker(place){
-    var marker = new google.maps.Marker({
-      position: place.coordin,
-      map: map,
-      animation: null
-    });
 
-
-  var infoWindow = new google.maps.InfoWindow({
-    content: '<div class="info"><div class="info-text"><p class="title">'+place.wikiTitle+'</p><p class="address">'+place.name+'</p></div><div class="bus-img"><img src="'+place.img+'" alt="place img"></div>'
-  });
-
-  //click listener
-  marjer.addListener('click', function(){
-    places().forEach(function(place){
-      place.infoWindow.close();
-    });
-    infoWindow.open(map, marker);
-    toggleBounce(marker);
-    setTimeout(function(){
-      marker.setAnimation(null);
-    }, 700);
-  });
-
-  // adding the info window and marker to the place
-  place.infoWindow = infoWindow;
-  place.marker= marker;
-}
+//   // creating a marker for the place
+//   // function addMarker(place){
+//   //   var marker = new google.maps.Marker({
+//   //     position: place.coordin,
+//   //     map: map,
+//   //     animation: null
+//   //   });
+//
+//
+//   var infoWindow = new google.maps.InfoWindow({
+//     content: '<div class="info"><div class="info-text"><p class="title">'+place.wikiTitle+'</p><p class="address">'+place.name+'</p></div><div class="bus-img"><img src="'+place.img+'" alt="place img"></div>'
+//   });
+//
+//   //click listener
+//   marker.addListener('click', function(){
+//     places().forEach(function(place){
+//       place.infoWindow.close();
+//     });
+//     infoWindow.open(map, marker);
+//     toggleBounce(marker); //<<<<<<<<<<<<<<<
+//     setTimeout(function(){
+//       marker.setAnimation(null);
+//     }, 700);
+//   });
+//
+//   // adding the info window and marker to the place
+//   place.infoWindow = infoWindow;
+//   place.marker= marker;
+// }
 
 //animation function for marker
-function toggleBounce(m){
-  if(m.getAnimation() !== null){
-    m.setAnimation(null);
-  } else {
-    m.setAnimation(google.maps.Animation.BOUNCE)
-  }
-}
+// function toggleBounce(m){
+//   if(m.getAnimation() !== null){
+//     m.setAnimation(null);
+//   } else {
+//     m.setAnimation(google.maps.Animation.BOUNCE);
+//   }
+// }
